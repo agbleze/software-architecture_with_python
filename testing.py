b6c8bee65c7151129e9636f8a50973156c74bdbb
linus <lagbleze@st.ug.edu.gh>
2024-03-05 23:29:01 +0100

    added get_url_data

--- testing.py
+++ testing.py
@@ -1,2 +1,9 @@
 import hashlib
 import requests
+
+def get_url_data(url):
+    """Return data for a URL"""
+    data = requests.get(url).content
+    filename = hashlib.md5(url).hexdigest()
+    open(filename, "w").write(data)
+    return data
