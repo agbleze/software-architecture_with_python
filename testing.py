e345cc8054007da6498afb12abb4d2b5af3a8207
linus <lagbleze@st.ug.edu.gh>
2024-03-06 23:39:40 +0100

    added get_url_data_stub

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 import hashlib
 import requests
+import os
 
 def get_url_data(url):
     """Return data for a URL"""
@@ -7,3 +8,9 @@ def get_url_data(url):
     filename = hashlib.md5(url).hexdigest()
     open(filename, "w").write(data)
     return data
+
+
+def get_url_data_stub(url):
+    filename = hashlib.md5(url).hexdigest()
+    if os.path.isfile(filename):
+        return open(filename).read()
